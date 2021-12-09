9bdb48912b61f85b7d233f1db63af042c768d11d
TohaRodiv <toha.rodiv@yandex.ru>
2021-12-09 23:51:43 +1000

    Добавил индикатор загрузки

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,12 @@ import "#styles/style.scss";
 import type { AppProps } from "next/dist/next-server/lib/router/router";
 import type { TMenu } from "#types/TMenu";
 import { NextPage } from "next";
-import React from "react";
+import React, { useEffect } from "react";
 import { Layout } from "#skeleton/Layout";
 import { mainMenu } from "#data/main-menu";
 import { categories } from "#data/categories";
+import NProgress from "nprogress";
+import { useRouter } from "next/router";
 
 
 type TProps = AppProps & {
@@ -24,6 +26,28 @@ const menu = {
 
 
 const Application: NextPage<TProps> = ({ Component, pageProps, }: TProps): JSX.Element => {
+	const router = useRouter();
+
+
+	useEffect(() => {
+		const handleStart = () => {
+			NProgress.start();
+		};
+
+		const handleStop = () => {
+			NProgress.done();
+		};
+
+		router.events.on("routeChangeStart", handleStart);
+		router.events.on("routeChangeComplete", handleStop);
+		router.events.on("routeChangeError", handleStop);
+
+		return () => {
+			router.events.off("routeChangeStart", handleStart);
+			router.events.off("routeChangeComplete", handleStop);
+			router.events.off("routeChangeError", handleStop);
+		};
+	}, [router]);
 
 	return (
 		<Layout menu={menu}>
